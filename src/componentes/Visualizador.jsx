8af11d8bc32460b2479497eb338ea740c1a121c3
igorFrotte <igorfrotte@gmail.com>
2025-09-12 10:43:27 -0300

    first view

--- src/componentes/Visualizador.jsx
+++ src/componentes/Visualizador.jsx
@@ -3,13 +3,64 @@ import git from "../assets/img/git.png";
 import styled from "styled-components"; 
 
 export default function Visualizador() {
+    const [inputValue, setInputValue] = useState("");
+    const [graph, setGraph] = useState({});
+    const [errorMsg, setErrorMsg] = useState("error");
 
+    function handleForm(e){
+        e.preventDefault();
+    
+        const num = parseInt(inputValue, 10);
+    
+        if (isNaN(num) || num < 5 || num > 20) {
+          setErrorMsg("Digite um número entre 5 e 20.");
+          setGraph({});
+        } else {
+          const obj = {};
+          setErrorMsg("");
+          for (let i = 0; i <= num; i++) {
+              obj[i] = {dest:1, cost:2, next:{dest:3, cost:4, next:null}};
+          }
+          setGraph(obj);
+        }
+    };
+
+    function showLine(line){
+        let value = <></>;
+        console.log(line);
+        while(line != null){
+            value = <>{value} {"  -->  "} <Edge><div>{line.dest}</div><div>{line.cost}</div></Edge></>;
+            line = line.next;
+        }   
+        return value; 
+    }
 
     return (
       <Page>
-        <Pointers>
-            <div>oblkgfnbklgfnlkfgbkngfbli</div>
-        </Pointers>
+        {errorMsg && <p>{errorMsg}</p>}
+
+        <form onSubmit={handleForm}>
+            <input
+            type="number"
+            min="5"
+            max="20"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            placeholder="Digite de 5 a 20"
+            />
+            <button type="submit"> Enviar </button>
+        </form>
+
+        <Graph>
+            {Object.entries(graph).map(([key, line]) => (
+                <div key={key}>
+                <Node>{key}</Node>
+                <Pointers>
+                    {showLine(line)}
+                </Pointers>
+                </div>
+            ))}
+        </Graph>
         <Git href="https://github.com/igorFrotte">
             <img src={git} />
         </Git>
@@ -43,28 +94,27 @@ const Page = styled.div`
     }
 `;
 
-const Pointers = styled.div`
+const Graph = styled.div`
+    display: flex;
+    flex-direction: column;
+
     & > div {
-        background-color: #447390;
-        width: 150px;
-        padding: 10px;
-        margin: 5px;
-        border-radius: 5px;
-        flex-direction: column;
+        display: flex;
+    }
+`;
 
-        & > div:first-child {
-            font-weight: bold;
-            font-size: 22px;
-        }
+const Pointers = styled.div`
+    border: 3px solid red;
+`;
 
-        & > div {
-            display: flex;
-            margin: 3px;
-            font-size: 18px;
+const Node = styled.div`
+    border: 3px solid black;
+`;
 
-            & > div {
-                padding: 0 5px;
-            }
-        }
+const Edge = styled.div`
+    border: 3px solid blue;
+
+    & > div {
+        border: 3px solid yellow;
     }
 `;
